c497f5dd0724b327afcbf9c0f846b01a42177054
Jay Chen <jay.chen.biz@gmail.com>
2021-06-15 17:15:07 +0800

    请求 Settings.ACTION_MANAGE_OVERLAY_PERMISSION

--- app/src/main/java/com/vitysoft/android/keeplandscape/MainActivity.kt
+++ app/src/main/java/com/vitysoft/android/keeplandscape/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.vitysoft.android.keeplandscape
 
-import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
-import android.util.TypedValue
-import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val textView by lazy {
+        findViewById<TextView>(R.id.textView)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,32 +21,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        checkPermission()
+        checkPermissions()
     }
 
-    private fun checkPermission() {
-        val textView: TextView
-        var intentSettings: Intent
-
-        if (!Settings.System.canWrite(this)) {
-            Toast.makeText(
-                this,
-                "Please select " + getString(R.string.app_name) + " and allow permissions first!",
-                Toast.LENGTH_LONG
-            ).show()
-
+    private fun checkPermissions() {
+        if (!Settings.System.canWrite(this)
+            || !Settings.canDrawOverlays(this)
+        ) {
+            Toast.makeText(this, R.string.request_permissions, Toast.LENGTH_LONG).show()
             if (!Settings.System.canWrite(this)) {
-                intentSettings = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
-                intentSettings.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intentSettings)
+                var settings = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+                settings.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(settings)
             }
-            textView = findViewById<View>(R.id.textView) as TextView
-            textView.text = "Required permissions not granted! Please restart this app"
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24f)
+            if (!Settings.canDrawOverlays(this)){
+                var settings = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                settings.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(settings)
+            }
+
+            textView.text = getString(R.string.no_permissions)
         } else {
-            textView = findViewById<View>(R.id.textView) as TextView
             textView.text = ""
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 0f)
 
             forceLandscape(this)
             startKeepAliveService(this)
